693153d7eb2b55c76b5974788d878824f3e15707
Iaggelis <john_agelis@hotmail.com>
2020-07-31 01:17:06 +0300

    Separate functionality with sql

--- src/TodoGrabber.jl
+++ src/TodoGrabber.jl
@@ -2,5 +2,12 @@ module TodoGrabber
 
 include("Todo.jl")
 include("grabber.jl");
+include("storingdb.jl")
+
+function todo_db(directory::String)
+    filenames = get_filenames("./tests")
+    todos = find_todos(filenames)
+    storetodos(todos)
+end # todo_db
 
 end # module
