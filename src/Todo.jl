cd2c60e26997926f75cd7918c222772f4aee6d72
Iaggelis <john_agelis@hotmail.com>
2020-07-31 01:14:34 +0300

    Changed the layout of the struct and new ctors

--- src/Todo.jl
+++ src/Todo.jl
@@ -4,10 +4,24 @@ mutable struct Todo
     Prefix::String
     Suffix::String
     Filename::String
-    Line::Int64
     Body::String
+    Line::Int64
     ID::Int64
 
-    # Todo(body) = new("", "", "", -1, body, -1) # initialize only body
+    Todo() = new("", "", "", "", -1, -1) # empty initializer
+    Todo(pre, suf, fn, body, ln, id) = new(pre, suf, fn, body, ln, id)
+    Todo(body) = new("", "", "", body, -1, -1) # initialize only body
 
+end
+
+function Base.:isempty(t::Todo)
+    flag = false
+    for i in 1:4
+        if isempty(getfield(t, i)) && !flag
+            flag = true
+            break
+        end
+    end # for
+
+    return flag
 end
