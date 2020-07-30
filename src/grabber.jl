0500677f6d89ec10f35c9c3092065e1f6fc8be0b
Iaggelis <john_agelis@hotmail.com>
2020-07-31 01:16:37 +0300

    new function to match lines of the file

--- src/grabber.jl
+++ src/grabber.jl
@@ -15,30 +15,31 @@ function get_filenames(fpath::String)
 end
 
 
-function write_todos(targetfile::String,
-                     tododict::Union{Dict{String, Vector{Todo}}, Nothing})
-    if tododict == nothing
-        printstyled(stderr, "No todos found", color = :red)
+function write_todos(tododict::Union{Dict{String,Vector{Todo}},Nothing},
+                     targetfile::String)
+    if tododict !== nothing
+        printstyled(stderr, "No todos found", color=:red)
         return nothing
     end
     open(targetfile, "w") do tfile
         for (fname, todo_vec) in tododict
             write(tfile, string("* ", fname, "\n"))
             for todo in todo_vec
-                write(tfile, string("** [[TODO]] ", todo.ID," ", todo.Suffix, "\n"))
+                write(tfile, string("** [[TODO]] ", todo.ID, " ", todo.Suffix,
+                                    "\n"))
             end
         end
     end
-
     return nothing
 end
 
-function save_dict(tododict::Dict{String, Vector{Todo}}; targetfile::String="")
+function save_dict(tododict::Dict{String,Vector{Todo}}; targetfile::String="")
     if isempty(targetfile)
         serialize(cachefile, tododict)
     else
         serialize(targetfile, tododict)
     end
+    return nothing
 end
 
 
@@ -48,12 +49,13 @@ function grab_dir(directory::String; target_file::String="")
     save_dict(todos)
     if isempty(target_file)
         printstyled(stderr, "No output file given. Writing on temp_stack.org",
-                    color = :yellow)
+                    color=:yellow)
         write_todos("./tests/temp_stack.org", todos)
     else
         println(stdout, "Writing on $target_file")
         write_todos(target_file, todos)
     end
+    return nothing
 end
 
 function find_todos(target_files::Vector{String})
@@ -61,7 +63,7 @@ function find_todos(target_files::Vector{String})
     for filename in target_files
         captured = Vector{Todo}(undef, 0)
         open(filename) do file
-            match_lines(captured, filename, file, todo_dict)
+            match_lines!(captured, filename, file, todo_dict)
         end # file loop
         if isempty(captured) # only push todos when they exist
             continue
@@ -75,21 +77,44 @@ function find_todos(target_files::Vector{String})
 end
 
 
-function match_lines(captured::Vector{Todo} , filename::String, file::IOStream,
-                     todo_dict::Union{Dict{String, Vector{Todo}}, Nothing})
+function match_lines!(captured::Vector{Todo}, filename::String, file::IOStream,
+                      todo_dict::Union{Dict{String,Vector{Todo}},Nothing})
 
-    for (line, line_text) in enumerate(eachline(file))
+    preffix = ""
+    line::Int64 = 0
+    sizet::Int64 = 0
+    search_body::Bool = false
+    temp_todo_vec = Vector{Todo}(undef, 256)
+    println(stdout, "working on ", filename)
+    while !eof(file)
+        line += 1
+        line_text = readline(file)
         m = match(r"^(.*)TODO: (.*)$", line_text)
         if m !== nothing
             # TODO: Rethink about stripping
             cleaned_capture = strip(m.captures[2], ['-','*','>',' ', '/'])
             if !haskey(todo_dict, filename) ||
-                isempty(findall(x->x==cleaned_capture, getindex(todo_dict, filename)))
-                push!(captured, Todo(m.captures[1],
-                                     String(cleaned_capture), filename,
-                                     line, "lmao", -1))
+                isempty(findall(x -> x == cleaned_capture, getindex(todo_dict,
+                                                                    filename)))
+                sizet += 1
                 preffix = m.captures[1]
+                temp_todo_vec[sizet] = Todo(preffix,
+                                              String(cleaned_capture),
+                                              filename,
+                                              "",
+                                              line,
+                                              sizet)
+
+                search_body = true
             end
-        end # regex if
-    end # line loop
-end
+        elseif !isempty(line_text) && !isempty(preffix) && search_body
+            if preffix == line_text[1:sizeof(preffix)]
+                temp_todo_vec[sizet].Body *= split(line_text, preffix)[2] * " "
+            else
+                preffix = ""
+                search_body = false
+            end
+        end
+    end # while loop
+    copy!(captured, temp_todo_vec[1:sizet])
+end # match_lines
